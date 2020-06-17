8ba504ca4d7fb433022f0fd7d8c6c221d4d961ce
iwatayusuke <mrnijigen@gmail.com>
2020-06-17 14:55:46 +0900

    課題提出 - 変数・定数

---
+++ variable/src/variable/Task2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		String name = "岩田　祐介";
+		final String TELEPHONE_NUMBER = "090-9999-1111";
+		System.out.println(name + "です。");
+		System.out.println("私の電話番号は\'" + TELEPHONE_NUMBER + "\'です。");
+	}
+
+}
