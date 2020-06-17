0671891fe325a05bec4b44d64034068e4c5e500f
iwatayusuke <mrnijigen@gmail.com>
2020-06-17 14:23:34 +0900

    課題提出 - Javaの基礎

---
+++ basic/src/basic/Task2.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// mainのブロックに処理を記述する
+		System.out.println("hogehoge");
+		System.out.println("hogehoge2");
+	}
+
+}
