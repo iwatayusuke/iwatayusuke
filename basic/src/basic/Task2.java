28f949a82942242ace448ca37142f84a478ef2f0
iwatayusuke <mrnijigen@gmail.com>
2020-06-18 13:58:59 +0900

    再提出 - Javaの基礎

--- basic/src/basic/Task2.java
+++ basic/src/basic/Task2.java
@@ -3,7 +3,8 @@ package basic;
 public class Task2 {
 
 	public static void main(String[] args) {
-		// mainのブロックに処理を記述する
+		// hogehogeとhogehoge2の記述が違うブロックに入っている
+		// どちらもmainのブロックに処理を記述する
 		System.out.println("hogehoge");
 		System.out.println("hogehoge2");
 	}
