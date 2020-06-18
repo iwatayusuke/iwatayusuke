e6aac3577330a7f65a3bfdf329065a9500fbbfc6
iwatayusuke <mrnijigen@gmail.com>
2020-06-18 14:00:39 +0900

    再提出 -変数・定数

--- variable/src/variable/Task3.java
+++ variable/src/variable/Task3.java
@@ -2,24 +2,24 @@ package variable;
 
 public class Task3 {
 
-	public static void main(String[] args) {
-		int intNum = 219;
-	    double doubleNum = 364.2;
-	    long longNum = 123456L;
+    public static void main(String[] args) {
+        int intNum = 219;
+        double doubleNum = 364.2;
+        long longNum = 123456L;
 
-	    long a = (long) intNum;
-	    float b = (float) doubleNum;
-	    double c = (double) longNum;
+        long a = (long) intNum;
+        float b = (float) doubleNum;
+        double c = (double) longNum;
 
-		System.out.println("変換前");
-		System.out.println(intNum);
-		System.out.println(doubleNum);
-		System.out.println(longNum);
+        System.out.println("変換前");
+        System.out.println(intNum);
+        System.out.println(doubleNum);
+        System.out.println(longNum);
 
-		System.out.println("変換後");
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-	}
+        System.out.println("変換後");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
 
 }
