ac04873bf74ad65662dbc742cbbc75a6cf139ffe
iwatayusuke <mrnijigen@gmail.com>
2020-07-22 16:27:23 +0900

    再提出 - サーブレット

--- servlet/src/jp/co/colla_tech/Task4.java
+++ servlet/src/jp/co/colla_tech/Task4.java
@@ -2,6 +2,7 @@ package jp.co.colla_tech;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,11 +14,16 @@ import javax.servlet.http.HttpServletResponse;
 public class Task4 extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html; charset = UTF-8");
         PrintWriter out = response.getWriter();
         HtmlTemplate.header(out);
+        out.println("<p>↓占い結果↓</p>");
+        String kuji;
+        Random r =new Random();
+        String[] fortune = new String[]{"大吉", "中吉", "小吉", "末吉", "凶", "大凶"};
+        kuji = fortune[r.nextInt(6)];
+        out.println("<h1>" + kuji + "</h1>");
         HtmlTemplate.footer(out);
     }
 
